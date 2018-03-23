9d701576dd31e6d43816ff63b319c91a7bbd4165
Nyarla4 <like98100@naver.com>
2018-03-23 09:11:23 +0900

    homework_3_ing2
    Q.3_ing2

--- 0321_homework_3/0321_homework_3/source.cpp
+++ 0321_homework_3/0321_homework_3/source.cpp
@@ -10,7 +10,7 @@ public:
 
 	CMyTime(int h, int m, int s);
 
-	add(CMyTime p);
+	void add(CMyTime p);
 	void print();
 };
 
@@ -18,14 +18,35 @@ CMyTime::CMyTime(int h, int m, int s) {
 	hour = h;
 	minute = m;
 	second = s;
+	//int exch_minute = 0;
+	//int exch_second = 0;
+	//while (1) {
+	//	if (second != 0 && second % 60 == 0) {
+	//		exch_second = 1;
+	//		second -= 60;
+	//		minute++;
+	//	}
+
+	//	if (minute != 0 && minute % 60 == 0) {
+	//		exch_minute = 1;
+	//		minute -= 60;
+	//		hour++;
+	//	}
+
+	//	if (exch_second == 1 && exch_minute == 1) break;
+	//	else {
+	//		exch_second == 0;
+	//		exch_minute == 0;
+	//	}
+	//}
 }
 
-CMyTime::add(CMyTime p) {
+void CMyTime::add(CMyTime p) {
 
 }
 
 void CMyTime::print() {
-
+	cout << hour << " : " <<  minute << " : " << second << endl;
 }
 int main() {
 
